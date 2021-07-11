9353277047e1a74fd31851daedaa901251133dde
Christian-Klempau <christian.klempau@uc.cl>
2021-07-10 22:13:26 -0400

    Repos downloader v1

---
+++ repos_downloader.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+# ---- Parámetros cambiables ----
+# Path al archivo CSV que contiene la información de los correctores
+path_csv = "datos.csv"
+# Tu nombre/usuario en el archivo CSV
+corrector = "Chris"
+# Subdirectorio a descargar: T3, AF6, AS2 ...
+actividad = "AF6"
+
+# URL absoluta al repositorio, no debería ser necesario cambiarla
+repo_base = r"https://github.com/IIC2233/"
+path_repo = repo_base + actividad
+
+
+def modo_automatico():
+    # Transforma el CSV a una lista de tuplas (corrector, alumno)
+    datos = [linea.strip().split(",") for linea in open(path_csv, "r", encoding="utf-8").readlines()]
+
+    # Comando inicial, crea el repositorio respectivo vacío, para poder agregarle contenido
+    git_setup = "git clone --depth 1 --filter=blob:none --sparse " + path_repo
+    os.system(git_setup)
+
+    # Itera sobre los alumnos y correctores, para agregar a la carpeta sólo las subcarpetas
+    for linea in datos:
+        corr, alumno = (linea[0], linea[1])
+        if(corr == corrector):
+            os.system(f"cd {actividad} && git sparse-checkout add {alumno}")
+
+
+def modo_manual(actividad_especifica, alumno):
+    print(f"Descargando {actividad_especifica} de {alumno}!")
+    git_setup = "git clone --depth 1 --filter=blob:none --sparse " + repo_base + actividad_especifica
+    os.system(git_setup)
+    os.system(f"cd {actividad_especifica} && git sparse-checkout add {alumno}")
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) == 4 and sys.argv[1] == "-i"):
+        print("Modo manual")
+        # argumentos: actividad y alumno
+        modo_manual(sys.argv[2], sys.argv[3])
+
+    elif(len(sys.argv) == 1):
+        print("Modo automático")
+        modo_automatico()
+    else:
+        print("ERROR: los argumentos no son correctos.")
